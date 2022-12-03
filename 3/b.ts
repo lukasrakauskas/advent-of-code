4fb38ddfe37b973c71fd8d19b37c89d5437adb04
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2022-12-03 14:41:23 +0200

    remove nonsense

--- 3/b.ts
+++ 3/b.ts
@@ -1,15 +1,6 @@
 const file = await Deno.readTextFile("./3/input.txt");
 const lines = file.split(/\r?\n/);
 
-type Tuple<T, N extends number> = N extends N
-  ? number extends N
-    ? T[]
-    : _TupleOf<T, N, []>
-  : never;
-type _TupleOf<T, N extends number, R extends unknown[]> = R["length"] extends N
-  ? R
-  : _TupleOf<T, N, [T, ...R]>;
-
 const isUppercase = (char: string) => char.toUpperCase() === char;
 
 const getPriority = (char: string) => {
@@ -20,12 +11,11 @@ const getPriority = (char: string) => {
   return char.charCodeAt(0) - 96;
 };
 
-function group<T>(arr: T[], n: 3 = 3) {
-  type TupledT = Tuple<T, typeof n>;
-  const group: Array<Tuple<T, typeof n>> = [];
+function group<T>(arr: T[], n: number) {
+  const group: T[][] = [];
 
   for (let i = 0, end = arr.length / n; i < end; ++i) {
-    group.push(arr.slice(i * n, (i + 1) * n) as TupledT);
+    group.push(arr.slice(i * n, (i + 1) * n));
   }
 
   return group;
